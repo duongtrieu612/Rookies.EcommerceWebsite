d95f18cb98451e6a0cec6150983c32615ef11c7a
Duong Trieu <duongtrieu54@gmail.com>
2022-10-25 22:31:08 +0700

    Api get by ID

--- Backend/Controllers/ProductsController.cs
+++ Backend/Controllers/ProductsController.cs
@@ -23,6 +23,19 @@ namespace Backend.Controllers
             return Json(dsProduct); 
         }
 
+        [HttpGet("{id:int}")]
+        public IActionResult GetProductById(int id)
+        {
+            var dsProduct = _context.Products.SingleOrDefault(x => x.Id == id);
+            return Json(dsProduct);
+        }
+        [HttpGet("{search}")]
+        public IActionResult SearchProduct(string search)
+        {
+            var dsProduct = _context.Products.Where(x => x.Name.Contains($"{search}")).ToList();
+            return Json(dsProduct);
+        }
+
 
     }
 }
