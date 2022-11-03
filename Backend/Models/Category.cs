b4e38954ac8b4d921602259a0ee9204b13cb4a0e
Duong Trieu <duongtrieu54@gmail.com>
2022-11-04 02:05:13 +0700

    Relocate Model

--- Backend/Models/Category.cs
+++ Backend/Models/Category.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Backend.Data
+namespace Backend.Models
 {
     [Table("Category")]
     public class Category
--- Backend/Models/Product.cs
+++ Backend/Models/Product.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Backend.Data
+namespace Backend.Models
 {
     [Table("Product")]
     public class Product
