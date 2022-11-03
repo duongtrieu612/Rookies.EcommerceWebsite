93ded5b8f3217e56091090d791a2450a1f732d89
Duong Trieu <duongtrieu54@gmail.com>
2022-11-04 02:04:39 +0700

    Add AutoMapper

---
+++ Backend/Mappings/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿    using AutoMapper;
+using Backend.Models;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Mappings
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CategoryViewModel, Category>();
+        }
+    }
+}
+
