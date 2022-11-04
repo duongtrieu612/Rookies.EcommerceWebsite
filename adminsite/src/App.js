460fd29116bb148f86035f1d795766d5896a2e15
Duong Trieu <duongtrieu54@gmail.com>
2022-11-04 23:49:29 +0700

    Load Category to table

--- adminsite/src/App.js
+++ adminsite/src/App.js
@@ -1,33 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import {Table} from 'react-bootstrap';
 
 function App() {
+  const [categories, setCategory] = useState([]);
+
+  function loadCategory() {
+    axios.get("https://localhost:7067/api/Category").then((res) => {
+      setCategory(res.data);
+    });
+  }
+
+  useEffect(() => {
+    loadCategory();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <button className='btn btn-secondary'>Add</button>
+      <Table className='table'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map(category =>
+            <tr key={category.id}>
+              <td>{category.id}</td>
+              <td>{category.name}</td>
+              <td> 
+                <button className='btn btn-info btn-sm'>Edit</button>
+                <button className='btn btn-danger btn-sm'>Delete</button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
     </div>
   );
 }
-axios.get('https://localhost:7067/api/Category')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
 export default App;
--- adminsite/src/index.js
+++ adminsite/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Container,Navbar } from 'react-bootstrap';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Admin Site</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">...</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
     <App />
   </React.StrictMode>
 );
