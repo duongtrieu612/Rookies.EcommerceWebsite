bc90084e0988231ab150a092715a0d8e5c93a667
Duong Trieu <duongtrieu54@gmail.com>
2022-11-05 22:50:53 +0700

    Add modal

--- adminsite/src/App.js
+++ adminsite/src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
-import {Table} from 'react-bootstrap';
+import {Table, Modal, Button} from 'react-bootstrap';
 
 function App() {
   const [categories, setCategory] = useState([]);
+  const [show, setShow] = useState(false);
+  const [deleteId, setdeleteId] = useState(null);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   async function loadCategory() {
     await axios.get("https://localhost:7067/api/Category").then((res) => {
@@ -13,9 +18,16 @@ function App() {
     });
   }
 
+  const DeleteClick = (id) =>{
+    setShow(true)
+    setdeleteId(id)
+  }
+    
+
   function DeleteCategory(id) {
      axios.delete(`https://localhost:7067/api/Category/${id}`)
      .then(() => {
+        setShow(false)
         loadCategory()
     });
   }
@@ -28,6 +40,20 @@ function App() {
 
   return (
     <div className='container'>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Xóa Danh mục này</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Bạn có chắc muốn xóa danh mục này không?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Hủy
+          </Button>
+          <Button variant="danger" onClick={()=>DeleteCategory(deleteId)}>
+            Xóa
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <button className='btn btn-secondary'>Add</button>
       <Table className='table'>
         <thead>
@@ -44,7 +70,7 @@ function App() {
               <td>{category.name}</td>
               <td> 
                 <button className='btn btn-info btn-sm'>Edit</button>
-                <button onClick={() => DeleteCategory(category.id)} className='btn btn-danger btn-sm' >Delete</button>
+                <button onClick={()=>DeleteClick(category.id)} className='btn btn-danger btn-sm' >Delete</button>                
               </td>
             </tr>
           )}
