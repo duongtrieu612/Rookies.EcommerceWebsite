e8b5bf38568dacac2e80322d3920f28922696b04
Duong Trieu <duongtrieu54@gmail.com>
2022-11-10 18:00:08 +0700

    Rating model

---
+++ Shared/ViewModels/RatingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.ViewModels
+{
+    public class RatingViewModel
+    {
+        public string Comment { get; set; }
+
+        public DateTime DateComment { get; set; }
+        public int RatingStar { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
